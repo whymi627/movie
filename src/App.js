58597ff6f1bd6609e4f4c03915cf01dcf629bd35
mi <whymi627@gmil.com>
2024-03-21 11:19:56 +0900

    d0321_1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((current) => current + 1);
+  console.log("I run all the time");
+  // #1
+  // const iRunOnlyOnce = () => {
+  //   console.log("I run only one.");
+  // };
+  // useEffect(iRunOnlyOnce, [])
+
+  // #2 익명함수로
+  useEffect(()=>{
+    console.log("I run only one.");
+  },[]);
   return (
     <div>
-
-        <h1 className={styles.title}>
-          Hello!!
-        </h1>
-        {/* <Button text= {12}/> */}
-        <Button text= {"Click me!"}/>
+        <input type="text" placeholder="Search here"></input>
+        <h1> {counter} </h1>
+        <button onClick={onClick}>click me</button>
+     
     </div>
        
   );
